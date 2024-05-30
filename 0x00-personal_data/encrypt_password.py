c45ba6020a1fc9eed9ce6031e79d439186402104
yusifhuseini <yusifhuseini@gmail.com>
2024-05-30 04:33:42 +0000

    0x00. Personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-"""A module for encrypting passwords.
+"""
+Hashing with bcrypt
 """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hashes a password using a random salt.
+    """
+    hash password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Checks is a hashed password was formed from the given password.
+    """
+    Validate password
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
